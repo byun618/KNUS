9e679459b8f60d976bf25f840f7890a644b5d667
byun618 <byun618@gmail.com>
2020-04-30 14:13:10 +0900

    Start the project

---
+++ KNUS/app/src/main/java/com/knu/knus/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.knu.knus;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new SplashHandler(), 3000);
+
+
+    }
+
+    private class SplashHandler implements Runnable{
+
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+}
