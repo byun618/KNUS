4bc360ec6c76d4c26cb212cad62ef958672b67c9
Sanghyun Byun <sanghyunbyun@Sanghyunui-MacBookPro.local>
2020-05-19 17:56:19 +0900

    Start Develop BoardView

--- KNUS/app/src/main/java/com/knu/knus/NoticeViewAdapter.java
+++ KNUS/app/src/main/java/com/knu/knus/NoticeViewAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -41,18 +42,29 @@ public class NoticeViewAdapter extends BaseAdapter {
 
         if(convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.notice_item, parent, false);
+            convertView = inflater.inflate(R.layout.test_item, parent, false);
         }
 
-        TextView title = convertView.findViewById(R.id.notice_title);
-        TextView subTitle = convertView.findViewById(R.id.notice_author);
-        TextView contents = convertView.findViewById(R.id.notice_content);
+//        TextView title = convertView.findViewById(R.id.notice_title);
+//        TextView subTitle = convertView.findViewById(R.id.notice_author);
+//        TextView contents = convertView.findViewById(R.id.notice_content);
+
+        ImageView content_profile = convertView.findViewById(R.id.content_profile);
+        TextView content_title = convertView.findViewById(R.id.content_title);
+        TextView content_when = convertView.findViewById(R.id.content_when);
+        TextView content_body = convertView.findViewById(R.id.content_body);
+
 
         NoticeViewItem noticeViewItem = noticeViewItems.get(position);
 
-        title.setText(noticeViewItem.getTitle());
-        subTitle.setText(noticeViewItem.getAuthor());
-        contents.setText(noticeViewItem.getContents());
+//        title.setText(noticeViewItem.getTitle());
+//        subTitle.setText(noticeViewItem.getAuthor());
+//        contents.setText(noticeViewItem.getContents());
+
+        content_profile.setImageResource(R.drawable.knu_logo);
+        content_title.setText(noticeViewItem.getTitle());
+        content_when.setText("10분전");
+        content_body.setText(noticeViewItem.getContents());
 
         return convertView;
     }
